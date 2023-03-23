e67c06a188649047565517ee518e6d9d0736b7a9
delso-ferreira <delsofelipe@gmail.com>
2023-03-23 19:44:30 -0300

    att requisito 4

--- src/main.js
+++ src/main.js
@@ -1,16 +1,55 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
-document.querySelector('.cep-button').addEventListener('click', searchCep);
 const productList = document.querySelector('.products');
 const getProducts = await fetchProductsList('computador');
+console.log(getProducts);
+const loadText = document.createElement('h1');
+const container = document.querySelector('.container');
+loadText.classList.add('loading');
+loadText.innerHTML = 'carregando...';
+document.querySelector('.cep-button').addEventListener('click', searchCep);
+/* const apiURL = 'https://api.mercadolibre.com/sites/MLB/search?q='; */
 
-getProducts.forEach((element) => {
-  const objeto = {
-    id: element.id,
-    title: element.title,
-    thumbnail: element.thumbnail,
-    price: element.price };
-  return createProductElement(objeto).appendChild(productList);
-});
+const requisitionError = () => {
+  const ReqError = document.createElement('h2');
+  ReqError.classList.add('error');
+  ReqError.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  container.appendChild(ReqError);
+};
+
+const productsReturn = () => {
+  getProducts.forEach((element) => {
+    const args = {
+      id: element.id,
+      title: element.title,
+      thumbnail: element.thumbnail,
+      price: element.price };
+    productList.appendChild(createProductElement(args));
+  });
+};
+productsReturn();
+
+const getLoadOn = () => {
+  container.appendChild(loadText);
+};
+
+const getLoadOff = () => {
+  container.removeChild(loadText);
+};
+
+const handleLoad = async () => {
+  getLoadOn();
+  if (getProducts === undefined) {
+    throw new Error('teste');
+  }
+  try {
+    await fetchProductsList('computador');
+    getLoadOff();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+handleLoad();
