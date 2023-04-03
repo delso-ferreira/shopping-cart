23ef76ace9d4e114acc33ef774dc97d502cca0d4
delso-ferreira <delsofelipe@gmail.com>
2023-04-03 10:24:53 -0300

    wip req 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,26 +1,38 @@
 export const getAddress = async (cep) => {
   const cartAdress = document.querySelector('.cart__address');
-  const awesomeCep = fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
-  const brasilCep = fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
+  const awesomeCep = await fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
+  const brasilCep = await fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
+  const statusOk = 200;
+  // statusOk se refere a resposta da API onde a chave status nas duas API tem o retorno 200
+  // caso exista erro a API retorna com 400 ou 404.
   let getAdress = '';
-  if (awesomeCep === undefined || brasilCep === undefined) {
-    throw new Error('CEP não encontrado');
-  }
   try {
+    if (awesomeCep.status !== statusOk || brasilCep.status !== statusOk) {
+      console.log(awesomeCep);
+      throw new Error('CEP não encontrado');
+    }
     await Promise.any([awesomeCep, brasilCep])
       .then((result) => result.json())
       .then((data) => {
-        getAdress = `${data.street} - ${data.neighborhood} - 
-    ${data.city} - ${data.state}`
-      || `${data.address} - ${data.district} - ${data.city} - ${data.state}`;
+        if (brasilCep) {
+          console.log('qualquerocoisa');
+          getAdress = `${data.street} - ${data.neighborhood} - 
+          ${data.city} - ${data.state}`;
+          cartAdress.innerHTML = getAdress;
+        }
+        if (awesomeCep) {
+          console.log('qualquerocoisa222');
+          getAdress = `${data.address} - ${data.district} - ${data.city} - ${data.state}`;
+          cartAdress.innerHTML = getAdress;
+        }
       });
-    cartAdress.innerHTML = getAdress;
   } catch (error) {
     cartAdress.innerHTML = error.message;
+    console.log(error.message);
   }
 };
 
 export const searchCep = async () => {
   const inputCep = document.querySelector('.cep-input');
-  await getAddress(inputCep.innerHTML);
+  await getAddress(inputCep.value);
 };
